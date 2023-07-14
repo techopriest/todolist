207eec2b073e8c7ba116c067bf282ad5e0cf6785
techopriest <techopriest@gmail.com>
2023-07-14 17:04:15 +1000

    edit task editting  commit

--- src/views/components/InputTask/index.tsx
+++ src/views/components/InputTask/index.tsx
@@ -11,19 +11,48 @@ interface InputTaskProps {
 }
 
 export const InputTask: React.FC<InputTaskProps> = ({
-//    id,
-//    title,
-//    onDone,
-//    onEdited,
-//    onRemoved
+    id,
+    title,
+    onDone,
+    onEdited,
+    onRemoved
 }) => {
-    return (
-        <div className={styles.inputPlus}>
-            <label>
-                <input
 
+    const [checked, setChecked] = useState(false);
+
+    return (
+        <div className={styles.inputTask}>
+            <label className={styles.inputTaskLabel}>
+                <input
+                    type="checkbox"
+                    checked={checked}
+                    className={styles.inputTaskChechbox}
+                    onChange={(evt) => {
+                        setChecked(evt.target.checked);
+
+                        if (evt.target.checked) {
+                            onDone(id);
+                        }
+                    }}
                 />
+                <h3 className={styles.inputTaskTitle}></h3>
             </label>
+            <button
+                aria-label="Edit"
+                className={styles.inputTaskEdit}
+                onClick={() => {
+
+                }}
+            />
+            <button
+                aria-label="Remove"
+                className={styles.inputTaskRemove}
+                onClick={() => {
+                    if(confirm('Are you sure?')) {
+                        onRemoved(id);
+                    }
+                }}
+            />
         </div>
     )
 }
